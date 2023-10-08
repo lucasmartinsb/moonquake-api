2fe9e7234a4b7f3dbeea385b8831db487053f1b0
Lucas Martins de Barros <00038997@randon.com.br>
2023-10-08 15:11:31 -0300

    [fix]

--- Extract.py
+++ Extract.py
@@ -47,7 +47,7 @@ class Extract:
             for row in data:
                 del row['_id']
                 pos = self.position(row['Timestamp'])
-                row['Date'] = row['Timestamp'].strftime('%Y/%m/%d')
+                row['Date'] = row['Timestamp'].strftime('%m/%d/%Y')
                 row['Timestamp'] = row['Timestamp'].strftime('%Y/%m/%d %H:%M:%S')
                 row['MoonphaseAngle'] = self.phase(pos)
             return json.dumps(data)
