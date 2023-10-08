d50663f2d04eb8833866d9b2846b6cb0947924df
Lucas Martins de Barros <00038997@randon.com.br>
2023-10-08 10:32:05 -0300

    [feat] adicionado cálculo de ângulo da lua

--- Extract.py
+++ Extract.py
@@ -2,17 +2,34 @@ import pandas as pd
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import json
-from bson import json_util
 from datetime import datetime
+import math, decimal
+
+dec = decimal.Decimal
 
 class Extract:
     def __init__(self, mongoConnectionString : str):
         self.client = MongoClient(mongoConnectionString, server_api=ServerApi('1'))  # URL de conexão com seu servidor MongoDB
-    
-    def json_serial(self, obj):
-        if isinstance(obj, datetime):
-            return obj.isoformat()
-        raise TypeError("Object of type {} is not JSON serializable".format(type(obj)))
+
+    def position(self, now : datetime): 
+        diff = now - datetime(2001, 1, 1)
+        days = dec(diff.days) + (dec(diff.seconds) / dec(86400))
+        lunations = dec("0.20439731") + (days * dec("0.03386319269"))
+        return lunations % dec(1)
+
+    def phase(self, pos): 
+        index = (pos * dec(8)) + dec("0.5")
+        index = math.floor(index)
+        return {
+            0: 0,   #"New Moon", 
+            1: 45,  #"Waxing Crescent", 
+            2: 90,  #"First Quarter", 
+            3: 135, #"Waxing Gibbous", 
+            4: 180, #"Full Moon", 
+            5: 225, #"Waning Gibbous", 
+            6: 270, #"Last Quarter", 
+            7: 315, #"Waning Crescent"
+        }[int(index) & 7]
 
 
     def extraction(self, db : str, collection : str) -> pd.DataFrame:
@@ -29,5 +46,7 @@ class Extract:
             data = [document for document in cursor]
             for row in data:
                 del row['_id']
+                pos = self.position(row['Timestamp'])
                 row['Timestamp'] = row['Timestamp'].strftime('%Y/%m/%d %H:%M:%S')
+                row['MoonphaseAngle'] = self.phase(pos)
             return json.dumps(data)
